223fdff83ef3c491af4e04b20b2ad00707a2148c
wjswldms <111005967+wjswldms@users.noreply.github.com>
2026-07-31 15:16:47 +0900

    [업로드] 광야어드벤처 지도 웹사이트_기초 파일
    지도 웹사이트 제작을 위한 기초 파일 업로드

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+/* Firebase 콘솔 > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성의 값을 아래에 붙여 넣으세요. */
+window.GWANGYA_FIREBASE_CONFIG = {
+  apiKey: "여기에_apiKey",
+  authDomain: "여기에_project.firebaseapp.com",
+  databaseURL: "https://여기에-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "여기에_projectId",
+  storageBucket: "여기에_project.appspot.com",
+  messagingSenderId: "여기에_messagingSenderId",
+  appId: "여기에_appId"
+};
