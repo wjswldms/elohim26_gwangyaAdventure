537a18678fddd80e5e546d7324761dd82d8434f6
wjswldms <111005967+wjswldms@users.noreply.github.com>
2026-08-13 03:19:32 +0900

    Update Firebase configuration 코드
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 /* Firebase 콘솔 > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성의 값을 아래에 붙여 넣으세요. */
 window.GWANGYA_FIREBASE_CONFIG = {
-  apiKey: "여기에_apiKey",
-  authDomain: "여기에_project.firebaseapp.com",
-  databaseURL: "https://여기에-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "여기에_projectId",
-  storageBucket: "여기에_project.appspot.com",
-  messagingSenderId: "여기에_messagingSenderId",
-  appId: "여기에_appId"
+  apiKey: "AIzaSyBMH-xJYC6r6OAZ3oKpmq7FzczUofLfwM4",
+  authDomain: "gwangyaadventure.firebaseapp.com",
+  databaseURL: "https://gwangyaadventure-default-rtdb.firebaseio.com",
+  projectId: "gwangyaadventure",
+  storageBucket: "gwangyaadventure.firebasestorage.app",
+  messagingSenderId: "859667830208",
+  appId: "1:859667830208:web:dcbaaf29efb4cd561be425"
 };
