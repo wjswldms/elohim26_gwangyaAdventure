2181040c4e15e80828d7e5fd0afbae750e319ca7
wjswldms <111005967+wjswldms@users.noreply.github.com>
2026-08-13 03:16:28 +0900

    v12.2 미션 설명 및 만나 지급 조 선택 개선
    미션설명 최종수정, 만나지급 자동선택 버그 개선

---
+++ gwangya_adventure_v12_2_15teams(1)/sw.js
@@ -0,0 +1,5 @@
+const CACHE='gwangya-adventure-v12-2-15teams';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./firebase-config.js','./map.jpg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
