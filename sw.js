15ea92b7637a4398d1cc9abba785420e90f83cf6
wjswldms <111005967+wjswldms@users.noreply.github.com>
2026-08-13 03:17:31 +0900

    v12.2 미션 설명 및 만나 지급 조 선택 개선
    미션설명 최종 수정 및 만나지급 조 자동선택 개선

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gwangya-adventure-v12';
+const CACHE='gwangya-adventure-v12-2-15teams';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./firebase-config.js','./map.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
